ca3b7247784978d4458abc2b46c639063232a333
Yurij Lyubchenko <lubchenko05@gmail.com>
2017-09-07 22:37:30 +0300

    Fix static media url

--- django_test/urls.py
+++ django_test/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/', include('api.urls')),
     url(r'^', include('root.urls')),
-
-]
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
